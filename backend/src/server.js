62c9f85cdc544359b26184651ebe5ee9f103498d
UsmanRizan <usmanrizan6@gmail.com>
2025-07-06 14:24:02 +0530

    for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,19 +11,32 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 console.log(process.env.MONGO_URI);
 
 // Middleware to parse JSON requests
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173", // Adjust this to your frontend URL
-  })
-);
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // Adjust this to your frontend URL
+    })
+  );
+}
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the React frontend app
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // Handle React routing, return all requests to React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
